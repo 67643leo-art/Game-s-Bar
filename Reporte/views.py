7025a55c62e3a55a28984d46267e90e5f6556c8a
67643leo-art <67643leo@universidaddeleon.edu.mx>
2026-02-21 14:54:39 -0600

    Cambio de hoy

--- Reporte/views.py
+++ Reporte/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def reporte (request): 
-    return render(render, "reporte.html")
+    return render(request, "reporte.html")
